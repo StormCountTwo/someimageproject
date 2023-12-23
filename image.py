8cede2bd7ce5ddb1b9628e0d621fe3e246f3c459
Penny <69146861+StormCountTwo@users.noreply.github.com>
2023-12-23 15:53:23 +0000

    Update image.py
    changed from text based to using tkinter for GUI

--- image.py
+++ image.py
@@ -1,11 +1,23 @@
-import os
-from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 
-image_name = input("Enter image name: ")
-if os.path.isfile(image_name) == True:
-    im = Image.open(image_name)
-    im.show()
-elif ("." in image_name) == False:
-    print("Please include a valid file type")
-else:
-    print("no")
+def image_popup():
+    user_image = filedialog.askopenfilename()
+    top = tk.Toplevel(root)
+    try:
+        im = Image.open(user_image)
+        top_image = ImageTk.PhotoImage(im)
+        tk.Label(top, image=top_image).pack()
+        top.mainloop()
+        print("valid image")
+    except:
+        print("error")
+
+
+root = tk.Tk()
+root.geometry('500x500')
+
+open_image = tk.Button(root, text="Open Image", command=image_popup).pack(pady='20')
+
+root.mainloop()
