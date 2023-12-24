8aafdd7c646d30bc1ff0e9e409bd7eb54b8e5e8f
Penny <69146861+StormCountTwo@users.noreply.github.com>
2023-12-24 10:22:00 +0000

    Update image.py

--- image.py
+++ image.py
@@ -2,22 +2,26 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
-def image_popup():
-    user_image = filedialog.askopenfilename()
-    top = tk.Toplevel(root)
-    try:
-        im = Image.open(user_image)
-        top_image = ImageTk.PhotoImage(im)
-        tk.Label(top, image=top_image).pack()
-        top.mainloop()
-        print("valid image")
-    except:
-        print("error")
+def main():
+    def image_popup():
+        user_image = filedialog.askopenfilename()
+        top = tk.Toplevel(root)
+        top.geometry('540x540')
+        try:
+            im = Image.open(user_image)
+            top_image = ImageTk.PhotoImage(im)
+            tk.Label(top, image=top_image).pack()
+            print(user_image)
+            top.mainloop()
+        except IOError:
+            print("error")
 
+    root = tk.Tk()
+    root.title("Image Viewer")
+    root.geometry('500x500')
+    open_image = (tk.Button(root, text="Open Image", command=image_popup))
+    open_image.pack(pady='20')
+    root.mainloop()
 
-root = tk.Tk()
-root.geometry('500x500')
-
-open_image = tk.Button(root, text="Open Image", command=image_popup).pack(pady='20')
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
